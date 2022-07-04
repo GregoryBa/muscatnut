6e06c05c4380b10be8ac087d17ee8d67db599f4c
GregoryBa <gregory.ba.95@gmail.com>
2022-07-04 11:54:06 +0200

    updated models

--- Muscatnut.API/Models/Ingredient.cs
+++ Muscatnut.API/Models/Ingredient.cs
@@ -9,7 +9,8 @@ public class Ingredient
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; } = Guid.NewGuid();
 
-    [Required] public string Name { get; set; }
+    [Required] 
+    public string Name { get; set; }
     public int Calories { get; set; }
     public double Protein { get; set; }
     public double Carbohydrates { get; set; }
